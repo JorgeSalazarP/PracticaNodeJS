b1f39c9c66cdd35180e63ffadad88659a29ea934
Jorge Salazar <j-salazarp@hotmail.com>
2021-02-05 23:15:07 +0100

    Personalizamos el milddleware de error de la app.js

--- Nodepop/routes/index.js
+++ Nodepop/routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 const Anuncio = require('../models/Anuncio');
 
+
 /* GET home page. */
-router.get('/', async function(req, res, next) {
+router.get('/',async function(req, res, next) {
 
   try {
     const tag = req.query.tag;
